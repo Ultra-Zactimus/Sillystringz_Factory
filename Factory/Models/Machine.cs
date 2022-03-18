6fd80d5b0863069418ace3a66401550297dd8f56
Ultra Zacky <ultrazacky@gmail.com>
2022-03-18 09:08:09 -0700

    add Context code and Migrations

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -9,7 +9,7 @@ namespace Factory.Models
 
     }
     public string Hardware { get; set; }
-    public int HardwareId { get; set; }
+    public int MachineId { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; }
   }
 }
